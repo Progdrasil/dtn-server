38b0940657d269f8f9bf4085b1d0188f6eaebadd
Rene Leveille <rene.leveille@polymtl.ca>
2021-11-25 16:22:12 -0500

    initial server

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		dbUrl = "postgresql://postgres:postgres@localhost:5432/postgres"
+	}
+
+	db, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(-1)
+	}
+
+	defer db.Close()
+	server := newServer(db)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	r.Get("/bundle/{id:[a-z]+}", server.getBundles)
+	r.Post("/bundle/{id:[a-z]+}", server.createBundle)
+	http.ListenAndServe(":"+port, r)
+}
+
+type Server struct {
+	db *sql.DB
+}
+
+func newServer(db *sql.DB) *Server {
+	return &Server{
+		db,
+	}
+}
+
+func (s *Server) getBundles(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (s *Server) createBundle(w http.ResponseWriter, r *http.Request) {
+	// id := chi.URLParam(r, "id")
+
+}
